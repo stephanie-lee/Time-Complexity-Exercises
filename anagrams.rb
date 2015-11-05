97324efd184b825c48885c0501da28e4fe0f6295
Stephanie Lee <lee.en.wei@gmail.com>
2015-11-05 14:37:23 -0800

    finished anagrams

---
+++ anagrams.rb
@@ -0,0 +1,70 @@
+def first_anagram?(str1, str2)
+  letters = str1.split("")
+  word_permutations = letters.permutation(letters.count).to_a.map(&:join)
+
+  word_permutations.include?(str2)
+end
+
+def second_anagram?(str1, str2)
+  str1_arr = str1.split('')
+  str2_arr = str2.split('')
+
+  until str2_arr.empty? || str1_arr.empty?
+    str1_arr.each_with_index do |letter, idx|
+      if letter == str2_arr.first
+        str1_arr.delete_at(idx)
+        str2_arr.shift
+        break
+      elsif letter != str2_arr.first && idx == str1_arr.count - 1
+        return false
+      end
+    end
+  end
+
+  str1_arr.empty? && str2_arr.empty?
+end
+
+def third_anagram?(str1, str2)
+  str1_arr = str1.split('').sort
+  str2_arr = str2.split('').sort
+
+  str1_arr == str2_arr
+end
+
+def fourth_anagram?(str1, str2)
+  str1_arr = str1.split('')
+  str2_arr = str2.split('')
+
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+
+  str1_arr.each do |letter1|
+    hash1[letter1] += 1
+  end
+
+  str2_arr.each do |letter2|
+    hash2[letter2] += 1
+  end
+  p hash1
+  p hash2
+  hash1 == hash2
+end
+
+def fifth_anagram?(str1, str2)
+  str1_arr = str1.split('')
+  str2_arr = str2.split('')
+
+  hash = Hash.new(0)
+
+  str1_arr.each do |letter1|
+    hash[letter1] += 1
+  end
+
+  str2_arr.each do |letter2|
+    hash[letter2] -= 1
+  end
+  p hash
+  hash.values.all?{|num| num == 0}
+end
+
+p fifth_anagram?("elvis", "lives")
